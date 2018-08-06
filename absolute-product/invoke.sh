05660c52bc0faf070fb4e6d7bdae210182d528a2
Marcus Legendre <marcus.legendre@gmail.com>
2018-08-06 14:24:45 +0200

    absolute-product: invoke with a second set of parameters

--- absolute-product/invoke.sh
+++ absolute-product/invoke.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo "Example invocation #1:"
+wsk action invoke absoluteProduct/main --result --param a 12.34 --param b 42
+
+echo "Example invocation #1:"
 wsk action invoke absoluteProduct/main --result --param a 14.2 --param b -2.3
